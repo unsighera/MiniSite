1fc69fd5cf2795c04c6c8dd816a2488c364daf88
unsighera <eddy.weston@bk.ru>
2024-04-22 00:38:31 +0300

    Добавьте файлы проекта.

---
+++ MiniSite/Data/UsersClass.cs
@@ -0,0 +1,26 @@
+﻿namespace MiniSite.Data
+{
+    public class UsersClass
+    {
+        public List<User> userList = new List<User>
+        {
+            new User("user1", "user1", "user1"),
+            new User("user2", "user2", "user2"),
+            new User("user3", "user3", "user3")
+        };
+    }
+    
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+
+        public User(string login, string password, string name)
+        {
+            Login = login;
+            Password = password;
+            Name = name;
+        }
+    }
+}
