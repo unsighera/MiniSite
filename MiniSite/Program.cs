6ac55dd127a1e9677d6c2d1e3c006ab0575acdab
unsighera <eddy.weston@bk.ru>
2024-04-24 01:13:45 +0300

    микро недочеты но все гуд

--- MiniSite/Program.cs
+++ MiniSite/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<DataContext>();
+builder.Services.AddSingleton<UserContext>();
 
 var app = builder.Build();
 
